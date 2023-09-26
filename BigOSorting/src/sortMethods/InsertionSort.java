84b0dcde754a561b63b860ebb723a622cf2a0b72
Tate Mouser <tatesmouser@gmail.com>
2023-09-25 23:25:30 -0500

    Added insertion sort for integer array.

--- BigOSorting/src/sortMethods/InsertionSort.java
+++ BigOSorting/src/sortMethods/InsertionSort.java
@@ -7,7 +7,28 @@ public class InsertionSort {
 		}
 	}
 	
+	/**
+	 * 	Consist of sorted array and unsorted array.
+	 * 	One element is added to sorted array and placed in correct order.
+	 * 	Continue until sorted array = array size.
+	 * 
+	 * 	O(n^2) occurs when an array is descendingly sorted prior Ex: [9 6 3 3 2 1 1]
+	 */
 	public void sort(Integer[] elements)  {
-		
+		// Loop used to restart from end of sorted array. Updating i.
+		for(int i = 1; i < elements.length; i++) {
+			
+			int j = i - 1;
+
+			Integer current = elements[i];
+			
+			// Voids start and loops until previous is less than current
+			while(j >= 0 && elements[j].compareTo(current) > 0) { // CompareTo Ex: 8,9 = 1 or 9,8 = -1
+				elements[j+1] = elements[j]; // Shift back current
+				j--;
+			}
+			// Place element
+			elements[j+1] = current;
+		}
 	}
 }
